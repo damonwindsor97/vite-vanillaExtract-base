095ef6a4a92a1a3e5c053c319ef49bf4e1c02f28
Damon Windsor <damonwindsor@gmail.com>
2024-01-18 01:10:06 +1100

    Base

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# vite-vanillaExtract-base
- Free-To-Use base Vite Project setup with Routes and Vanilla Extract
+# React + Vite + Vanilla Extract base
+
+<ul> <h3>Pre-Installed Packages</h3>
+    <li>React</li>
+    <li>Vite</li>
+    <li>react-router-dom</li>
+    <li>Vanilla Extract CSS</li>
+</ul>
---
+++ src/styles/themes.css.ts
@@ -0,0 +1,28 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+
+export const vars = createGlobalTheme(':root', {
+    fonts: {
+
+    },
+
+
+    fontWeight: {
+        light: "300",
+        normal: "500",
+        bold: "600",
+        bolder: "700",
+    },
+
+
+    colors: {
+
+    },
+
+    space: {
+        none: '0',
+        '1x': '8px',
+        '2x': '12x',
+        '3x': '16px'
+    }
+})
